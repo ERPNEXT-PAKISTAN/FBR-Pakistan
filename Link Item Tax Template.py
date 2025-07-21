d9f164e1fae96c87343a7138f929c81f733ad2ab
ERPNEXT-PAKISTAN <79786408+ERPNEXT-PAKISTAN@users.noreply.github.com>
2025-07-21 22:35:54 +0500

    Update Link Item Tax Template.py

--- Link Item Tax Template.py
+++ Link Item Tax Template.py
@@ -1,11 +1,11 @@
 # this script is used to link item tax template with custom fiel create on delivery note item table.
-# field name is "custom_taxs_rate" and "custom_taxs_amount"
-
+# field name is "custom_taxs_rate" and "custom_taxs_amount" & "custom_tax_inclusive"
 
 
 for item in doc.items:
     item.custom_taxs_rate = 0
     item.custom_taxs_amount = 0
+    item.custom_tax_inclusive = 0
 
     if item.item_tax_template:
         tax_details = frappe.get_all("Item Tax Template Detail",
@@ -14,9 +14,8 @@ for item in doc.items:
         )
 
         if tax_details:
-            # You can also loop if there are multiple tax types
             item.custom_taxs_rate = tax_details[0].tax_rate or 0
 
-            # Calculate tax amount from item amount
             if item.amount:
                 item.custom_taxs_amount = (item.amount * item.custom_taxs_rate) / 100
+                item.custom_tax_inclusive = item.amount + item.custom_taxs_amount
