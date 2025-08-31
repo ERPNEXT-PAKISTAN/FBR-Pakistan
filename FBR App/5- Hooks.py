d17f603b7de56b533d90053ca44bb7144231e9b6
ERPNEXT-PAKISTAN <79786408+ERPNEXT-PAKISTAN@users.noreply.github.com>
2025-08-31 21:50:41 +0500

    Create 5- Hooks.py

---
+++ FBR App/5- Hooks.py
@@ -0,0 +1,15 @@
+app_name = "fbr_integration"
+app_title = "FBR Integration"
+app_publisher = "Taimoor"
+app_description = "FBR Digital Invoice Integration"
+app_email = "you@example.com"
+app_license = "MIT"
+
+# include client side JS (this path is relative to /assets after build)
+app_include_js = "/assets/fbr_integration/js/sales_invoice.js"
+
+doc_events = {
+    "Sales Invoice": {
+        "on_submit": "fbr_integration.fbr_api.after_submit_invoice"
+    }
+}
