ec323ab04952e3fbffabbf15e80130c63d96fbea
ERPNEXT-PAKISTAN <79786408+ERPNEXT-PAKISTAN@users.noreply.github.com>
2025-09-04 23:36:27 +0500

    Update and rename FBR Tax Calculation.py to FBR Invoice Send.js

---
+++ FBR App/FBR Invoice Send.js
@@ -0,0 +1,39 @@
+# Script Type = DocType Event
+# Reference Document Type = Sales Invoice
+# DocType Event = Berore Save
+
+
+frappe.ui.form.on("Sales Invoice", {
+    refresh: function(frm) {
+        // Only show button for submitted invoices and not already sent
+        if (frm.doc.docstatus === 1 && !frm.doc.custom_fbr_invoice_no) {
+            frm.add_custom_button(
+                __("Send to FBR"),
+                function() {
+                    frappe.call({
+                        method: "fbr_integration.handler.send_to_fbr_si",
+                        args: {
+                            name: frm.doc.name,
+                        },
+                        freeze: true,
+                        callback: function(r) {
+                            if (r.exc) {
+                                frappe.msgprint(
+                                    __("Error sending to FBR: {0}", [r.exc])
+                                );
+                            } else if (r.message) {
+                                frappe.msgprint(
+                                    __("FBR response: {0}", [
+                                        JSON.stringify(r.message),
+                                    ])
+                                );
+                                frm.reload_doc();
+                            }
+                        },
+                    });
+                },
+                __("FBR")
+            );
+        }
+    },
+});
