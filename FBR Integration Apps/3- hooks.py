b1a7820c0f6723289129e34430454f1c758f26f3
ERPNEXT-PAKISTAN <79786408+ERPNEXT-PAKISTAN@users.noreply.github.com>
2025-09-15 22:25:10 +0500

    Comment out app_include_js and doc_events

--- FBR Integration Apps/3- hooks.py
+++ FBR Integration Apps/3- hooks.py
@@ -6,10 +6,16 @@ app_email = "tymuur@outlook.com"
 app_license = "MIT"
 
 # include client side JS (this path is relative to /assets after build)
-app_include_js = "/assets/fbr_integration/js/sales_invoice.js"
+# app_include_js = "/assets/fbr_integration/js/sales_invoice.js"
 
-doc_events = {
-    "Sales Invoice": {
-        "on_submit": "fbr_integration.fbr_api.after_submit_invoice"
-    }
-}
+# To Send On Submit the Active This..........
+# doc_events = {
+#    "Sales Invoice": {
+#        "on_submit": "fbr_integration.fbr_api.after_submit_invoice"
+#    }
+#}
+
+# To Send on Button "Send to FBR" use this.........
+# if you want to send on Submitt then OFF thiss and ON above statement...........
+
+doc_events = {}
